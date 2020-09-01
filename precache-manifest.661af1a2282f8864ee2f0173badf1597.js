d8527824de67543ecf3cee3839480a41f3efc3fc
Amer Atui <ameratui03gmail.com>
2020-09-01 14:43:06 +0300

    Updates

---
+++ precache-manifest.661af1a2282f8864ee2f0173badf1597.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a4742b439a75d32fce5bf979f1f764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79db1bf1783de416da45",
+    "url": "/static/css/main.7930b06a.chunk.css"
+  },
+  {
+    "revision": "1d8e47bf42f55f26fd70",
+    "url": "/static/js/2.69729dab.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.69729dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79db1bf1783de416da45",
+    "url": "/static/js/main.a1b616d4.chunk.js"
+  },
+  {
+    "revision": "08cfe112bd9fdd5e2530",
+    "url": "/static/js/runtime-main.e9db74a9.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "fc513ba6159ca9e422cbd7ca0b1f048f",
+    "url": "/static/media/adidas-img.fc513ba6.png"
+  },
+  {
+    "revision": "3f0a6a833248a2f925c060f2970e8906",
+    "url": "/static/media/cart_icon.3f0a6a83.svg"
+  },
+  {
+    "revision": "b97fdecc6209d9d777205e723b9a20d4",
+    "url": "/static/media/converse-img.b97fdecc.png"
+  },
+  {
+    "revision": "03878ec2817a40ec1325b818706f7a5f",
+    "url": "/static/media/facebook_icon.03878ec2.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/static/media/instagram_icon.bb026c4d.svg"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/static/media/left_arrow.6c9efe0c.svg"
+  },
+  {
+    "revision": "5aec495c6e920fe94cfcd8e9ad8a452b",
+    "url": "/static/media/nike-img.5aec495c.png"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/static/media/right_arrow.be237328.svg"
+  }
+]);
