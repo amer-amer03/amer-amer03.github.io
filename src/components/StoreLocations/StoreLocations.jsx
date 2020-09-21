48216c1e1f866b948d5bff0350d5c92b27a5b17e
Amer Atui <ameratui03gmail.com>
2020-09-22 02:54:41 +0300

    added new components and styles

---
+++ src/components/StoreLocations/StoreLocations.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import classes from "./StoreLocations.module.scss";
+
+const StoreLocations = () => {
+  return (
+    <div className={classes.storeLocations}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        praesentium aliquid quis nihil, ipsum ea alias harum recusandae error
+        aperiam minima tempore pariatur et facere. Omnis beatae, atque iusto
+        odio cupiditate officiis, vitae placeat dolor eos ipsum excepturi, culpa
+        dolorem suscipit illum est minus? Deserunt quaerat quasi odit quae
+        delectus voluptatem aspernatur numquam repudiandae.
+      </p>
+      <p>
+        Exercitationem suscipit, laborum quam minima sapiente provident quo
+        aperiam consequatur dolor amet architecto. Distinctio aliquid quidem
+        nisi officia nemo, dolor architecto commodi repellendus recusandae iusto
+        vitae provident sint. Tempore, vitae sapiente! Illo quae, quis numquam
+        provident, blanditiis molestias impedit recusandae ex tenetur iure
+        commodi labore possimus laboriosam quisquam veritatis omnis quos
+        voluptates doloremque! Ipsam quia unde quisquam repudiandae illo?
+        Commodi distinctio omnis esse natus ea, porro provident doloribus
+        impedit amet, reprehenderit facere accusamus ratione, labore blanditiis
+        exercitationem! Mollitia, tempore dolores hic quae cum facere impedit
+        nihil ad deserunt tenetur alias, libero perspiciatis? Necessitatibus,
+        rem officia. Nemo!
+      </p>
+      <div>
+        <img
+          className={classes.storeLocationsImage}
+          src="https://i.imgur.com/NbmDlia.jpg"
+          alt="store"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default StoreLocations;
