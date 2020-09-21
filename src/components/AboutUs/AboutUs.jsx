5ab028c979afc5369dffb093041c0a367edc2173
Amer Atui <ameratui03gmail.com>
2020-09-22 02:33:47 +0300

    added about us component

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,29 +1,37 @@
 import React from "react";
+import classes from "./AboutUs.module.scss";
 
 const AboutUs = () => {
   return (
-    <div>
+    <div className={classes.aboutUs}>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
         praesentium aliquid quis nihil, ipsum ea alias harum recusandae error
         aperiam minima tempore pariatur et facere. Omnis beatae, atque iusto
         odio cupiditate officiis, vitae placeat dolor eos ipsum excepturi, culpa
         dolorem suscipit illum est minus? Deserunt quaerat quasi odit quae
-        delectus voluptatem aspernatur numquam repudiandae. Exercitationem
-        suscipit, laborum quam minima sapiente provident quo aperiam consequatur
-        dolor amet architecto. Distinctio aliquid quidem nisi officia nemo,
-        dolor architecto commodi repellendus recusandae iusto vitae provident
-        sint. Tempore, vitae sapiente! Illo quae, quis numquam provident,
-        blanditiis molestias impedit recusandae ex tenetur iure commodi labore
-        possimus laboriosam quisquam veritatis omnis quos voluptates doloremque!
-        Ipsam quia unde quisquam repudiandae illo? Commodi distinctio omnis esse
-        natus ea, porro provident doloribus impedit amet, reprehenderit facere
-        accusamus ratione, labore blanditiis exercitationem! Mollitia, tempore
-        dolores hic quae cum facere impedit nihil ad deserunt tenetur alias,
-        libero perspiciatis? Necessitatibus, rem officia. Nemo!
+        delectus voluptatem aspernatur numquam repudiandae.
+      </p>
+      <p>
+        Exercitationem suscipit, laborum quam minima sapiente provident quo
+        aperiam consequatur dolor amet architecto. Distinctio aliquid quidem
+        nisi officia nemo, dolor architecto commodi repellendus recusandae iusto
+        vitae provident sint. Tempore, vitae sapiente! Illo quae, quis numquam
+        provident, blanditiis molestias impedit recusandae ex tenetur iure
+        commodi labore possimus laboriosam quisquam veritatis omnis quos
+        voluptates doloremque! Ipsam quia unde quisquam repudiandae illo?
+        Commodi distinctio omnis esse natus ea, porro provident doloribus
+        impedit amet, reprehenderit facere accusamus ratione, labore blanditiis
+        exercitationem! Mollitia, tempore dolores hic quae cum facere impedit
+        nihil ad deserunt tenetur alias, libero perspiciatis? Necessitatibus,
+        rem officia. Nemo!
       </p>
       <div>
-        <img src="https://i.imgur.com/8oTP0BC.jpg" alt="store" />
+        <img
+          className={classes.aboutUsImage}
+          src="https://i.imgur.com/8oTP0BC.jpg"
+          alt="store"
+        />
       </div>
     </div>
   );
