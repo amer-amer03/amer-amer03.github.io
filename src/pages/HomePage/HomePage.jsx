1ade41b90201c86ef58b97f229d85ed31670fa94
Amer Atui <ameratui03gmail.com>
2020-09-22 02:58:46 +0300

    moved images from slider here

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,10 +4,16 @@ import Catalogue from "../../components/Catalogue/Catalogue";
 import classes from "./HomePage.module.scss";
 
 const HomePage = () => {
+  const images = [
+    "https://i.imgur.com/rDznWXr.jpg",
+    "https://i.imgur.com/u8hgTTv.jpg",
+    "https://i.imgur.com/gB5YmGx.jpg",
+    "https://i.imgur.com/0mmUYV2.jpg",
+  ];
   return (
     <div className={classes.homePage}>
       <div>
-        <Slider />
+        <Slider images={images} autoPlay={10} />
       </div>
       <Catalogue />
     </div>
